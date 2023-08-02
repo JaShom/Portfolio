83c900291abcd603bb783fa014f50f1b32c09b56
Awwal <jubral14@gmail.com>
2023-08-02 22:49:54 +0100

    a bit off css fix here nd there

--- myportfolio/src/components/homeComp/homeItems.component.tsx
+++ myportfolio/src/components/homeComp/homeItems.component.tsx
@@ -8,12 +8,12 @@ function HomeItems(): JSX.Element {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
                 console.log(entry)
-                // entry.target.classList.toggle('show', entry.isIntersecting);
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('show');
-                } else {
-                    entry.target.classList.remove('show');
-                }
+                entry.target.classList.toggle('show', entry.isIntersecting);
+                // if (entry.isIntersecting) {
+                //     entry.target.classList.add('show');
+                // } else {
+                //     entry.target.classList.remove('show');
+                // }
             });
         });
 
